2c862513b3c37c2f48a1e5e076bd81c87102b5f3
Unknown <bonnjona@hotmail.fr>
2017-12-16 11:02:56 +0100

    Added class Labels + tests

--- python/file_reader.py
+++ python/file_reader.py
@@ -2,40 +2,49 @@ import os
 import settings
 
 
+class Labels:
+
+    def __init__(self):
+        labels = open(settings.DEV_LABELS, 'r').read().split('\n')
+        dict_labels = dict()
+        for elem in labels:
+            parts = elem.split('_')
+            name = '_'.join(parts[:-1])
+            if name is not '':
+                dict_labels[name] = parts[-1]
+        self.dict_labels = dict_labels
+
+    def get_label(self, id):
+        """
+        :param id: Recording identifier.
+        :type id: str
+        :return: Label of the given recording.
+        :rtype str
+        """
+        return self.dict_labels[id]
+
+
 class FileReader:
 
     EXCLUDED = ['excluded_region', 'inter_segment_gap']
 
     def read_files(self):
-        output_file = open(settings.TRANS_MAN + '/dev.csv', 'w')
-        labels = open(settings.DEV_LABELS, 'r').read().split('\n')
-        for file in os.listdir(settings.TRANS_MAN_DEV_TXT):
-            file_path = os.path.join(settings.TRANS_MAN_DEV_TXT, file)
-            # print(file_path)
-            file_content = open(file_path).read()
-            text = ''
-            for line in file_content.split('\n'):
-                if not line.startswith(";;") and line:
-                    parts = line.split('>')
-                    infos = parts[0].split()
-                    # print(infos)
-                    if len(infos) > 2 and infos[2] not in self.EXCLUDED:
-                        text += parts[1]
-                    # if parts[2] != self.EXCLUDED:
-                    #    print(parts)
-            # print(infos[0])
-            # print(text)
+        """
+        Creates of csv file with 3 columns (ID, TEXT, LABEL), each line represents an audio transcription.
+        """
+        with open(settings.TRANS_MAN + '/dev.csv', 'w') as output_file:
 
-            # TODO separate class that returns a dict with <ID: LABEL>
-            # get classes
-            for elem in labels:
-                parts = elem.split('_')
-                name = '_'.join(parts[:-1])
-                print(name)
-                if infos[0] == name:
-                    class_val = parts[-1]
-            output_file.write('{}\t{}\t{}\n'.format(infos[0], text, class_val))
-        output_file.close()
+            labels = Labels()
+            for file in os.listdir(settings.TRANS_MAN_DEV_TXT):
+                file_path = os.path.join(settings.TRANS_MAN_DEV_TXT, file)
+                # print(file_path)
+                file_content = open(file_path).read()
+                text = ''
+                for line in file_content.split('\n'):
+                    if not line.startswith(";;") and line:
+                        parts = line.split('>')
+                        infos = parts[0].split()
+                        if len(infos) > 2 and infos[2] not in self.EXCLUDED:
+                            text += parts[1]
 
-    def test(self):
-        pass
+                output_file.write('{}\t{}\t{}\n'.format(infos[0], text, labels.get_label(infos[0])))
--- python/tests/test_file_reader/test_file_reader.py
+++ python/tests/test_file_reader/test_file_reader.py
@@ -2,6 +2,18 @@ import unittest
 import file_reader as fr
 
 
+class TestLabels(unittest.TestCase):
+
+    def setUp(self):
+        self.labels = fr.Labels()
+
+    def test_init(self):
+        self.assertEqual(298, len(self.labels.dict_labels))
+
+    def test_get_label_when_id_exists_expect_label(self):
+        self.assertEqual('NVGO', self.labels.get_label('20091112_RATP_SCD_0096'))
+
+
 class TestFileReader(unittest.TestCase):
 
     def test_read_file(self):

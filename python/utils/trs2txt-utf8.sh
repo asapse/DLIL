7bdf38fd5c9c8dcc5eaae8826531548ad81f987a
Unknown <bonnjona@hotmail.fr>
2017-12-22 09:52:59 +0100

    Added README + generic script to gen trs to txt

--- python/utils/trs2txt-utf8.sh
+++ python/utils/trs2txt-utf8.sh
@@ -1,8 +1,12 @@
-# give folder in parameter (e.g. '../../release1/trans-man/dev'
+# give folder in parameter (e.g. 'trans-manu/dev')
 
-for file in `ls $1/*.trs`; do
+if [ ! -d ../../release1/$1/txt ]; then
+    mkdir ../../release1/$1/txt
+fi
+for file in `ls ../../release1/$1/*.trs`; do
 	filename=`basename $file .trs`
 	perl trs2stm.pl $file > $filename.txt
-	iconv -f iso-8859-15 -t utf-8 $filename.txt > $1/txt/$filename.utf8.txt
+	iconv -f iso-8859-15 -t utf-8 $filename.txt > ../../release1/$1/txt/$filename.utf8.txt
 	rm -rf *.txt
 done
+echo 'Done! Find files in '../../release1/$1/txt/

7d7dea548b79e358699645e213faf42b56b14d87
David Kerbrat <david.kerbrat@gmail.com>
2018-01-16 09:41:43 +0100

    implemantation de l'approche de base dans le main

---
+++ python/classifiers/basic.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, f1_score, recall_score, precision_score
+from sklearn.model_selection import cross_validate
+
+class BasicClf:
+
+    def __init__(self, corpus_train):
+        self.clf = MultinomialNB(alpha=.1)
+        self.vectorizer = CountVectorizer()
+        self.X_train = self.extract_features(pd.DataFrame(corpus_train)['content'], fit=True)
+        self.y_train = pd.DataFrame(corpus_train)['label']
+        self.train()
+
+    def extract_features(self, X, fit=False):
+        if fit is True:
+            return self.vectorizer.fit_transform(X)
+        else:
+            return self.vectorizer.transform(X)
+
+    def train(self):
+        self.clf.fit(self.X_train, self.y_train)
+
+    def evaluate(self, corpus_dev):
+        X_dev = self.extract_features(pd.DataFrame(corpus_dev)['content'])
+        y_dev = pd.DataFrame(corpus_dev)['label']
+        
+        y_pred = self.clf.predict(X_dev)
+
+        f1_micro = f1_score(y_dev, y_pred, average='micro')
+        f1_macro = f1_score(y_dev, y_pred, average='macro')
+        recall_macro = recall_score(y_dev, y_pred, average='macro')
+        precision_macro = precision_score(y_dev, y_pred, average='macro')
+        precision_micro = precision_score(y_dev, y_pred, average='micro')
+
+        return {'f1_macro': f1_macro, 'f1_micro': f1_micro, 'recall_macro': recall_macro, 'precision_macro': precision_macro, 'precision_micro': precision_micro}
+
+    def predict(self):
+        pass
--- python/main.py
+++ python/main.py
@@ -3,30 +3,39 @@ import os
 from pathlib import Path
 import pandas as pd
 from csv_generator import CSVGenerator
+from sklearn.naive_bayes import MultinomialNB
+from classifiers.basic import BasicClf
 
 
-def csv_exist(trans,file):
-	path = Path("../release1/trans-"+trans+"/"+file+".csv")
-	if path.exists():
-		return pd.read_csv(path, sep='\t', header=None, names=['id', 'content', 'label'], quoting=3, keep_default_na=False)
-	else:
-		txt_exist(trans,file)
-		csvg = CSVGenerator("trans-"+trans+"/"+file)
-		csvg.generate()
-		return pd.read_csv(path, sep='\t', header=None, names=['id', 'content', 'label'], quoting=3, keep_default_na=False)
+def csv_exist(trans, file):
+    path = Path("../release1/trans-" + trans + "/" + file + ".csv")
+    if path.exists():
+        return pd.read_csv(path, sep='\t', header=None, names=['id', 'content', 'label'], quoting=3, keep_default_na=False)
+    else:
+        txt_exist(trans, file)
+        csvg = CSVGenerator("trans-" + trans + "/" + file)
+        csvg.generate()
+        return pd.read_csv(path, sep='\t', header=None, names=['id', 'content', 'label'], quoting=3, keep_default_na=False)
 
-def txt_exist(trans,file):
-	file_path = "../release1/trans-"+trans+"/"+file+"/txt/"
-	path = Path(file_path)
-	if not path.exists() or os.listdir(file_path) == []:
-		os.popen("sh utils/trs2txt-utf8.sh trans-"+trans+"/"+file, "r").read()
+
+def txt_exist(trans, file):
+    file_path = "../release1/trans-" + trans + "/" + file + "/txt/"
+    path = Path(file_path)
+    if not path.exists() or os.listdir(file_path) == []:
+        os.popen("sh utils/trs2txt-utf8.sh trans-" +
+                 trans + "/" + file, "r").read()
 
 
 def main(argv):
-	if argv[1]=="manu":
-		train_corpus = csv_exist(argv[1], "train")
-		dev_corpus = csv_exist(argv[1], "dev")
-	if argv[1]=="auto":
-		pass
+    if argv[1] == "manu":
+        train_corpus = csv_exist(argv[1], "train")
+        dev_corpus = csv_exist(argv[1], "dev")
+    if argv[1] == "auto":
+        pass
+
+    basic = BasicClf(train_corpus)
+    print(basic.evaluate(dev_corpus))
+
+
 if __name__ == "__main__":
-   	 main(sys.argv)
+    main(sys.argv)
